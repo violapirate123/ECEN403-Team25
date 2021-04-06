7d97feab47fae2ce96fe83f43a28b2fd7e432bca
jmanrique9 <72229670+jmanrique9@users.noreply.github.com>
2021-04-06 16:15:39 -0500

    Add files via upload

---
+++ rodmotorcode.ino
@@ -0,0 +1,20 @@
+const int stepperpin = 8; //stepper pin of rod motor set to pin 8.
+const int driverpin = 9; //driver pin of rod motor set to pin 9.
+
+void setup() {
+ pinMode(8,OUTPUT); // pin 8 set as output.
+ pinMode(9,OUTPUT); // pin 9 set as output.
+
+}
+
+void loop() {
+  digitalWrite(driverpin,HIGH); //initiates motor rotation.
+
+  for(int i = 0; i < 200; i++){ //loop that will send 200 pulses to stepper motor(this depends on step angle of 1.8 degrees.
+    digitalWrite(stepperpin,LOW); //high part of pulse.
+    delayMicroseconds(1); //very small delay for maximum speed,it can be adjusted if you find issues.
+    digitalWrite(stepperpin,LOW); //low part of pulse.
+    delayMicroseconds(1); //very small delay for maximum speed,it can be adjusted if you find issues.
+  }
+
+}
